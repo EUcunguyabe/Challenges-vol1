749184e40395ccea91ba8ce6af7c277ee889d1ef
emmelance <e.ucunguyab@alustudent.com>
2023-12-14 16:24:24 +0200

    challenge 7

--- challenge7.py
+++ challenge7.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 #count number of vowels in a string
 string=str(input("Enter any word:"))
-vowels="AaIiUuEeOo"
+#vowels="AaIiUuEeOo"
 count=0
 for i in string:
-    if i in vowels:
+    if(i=="a" or i=="A" or i=="I" or i=="i" or i=="u" or i=="U" or i=="e" or i=="E" or i=="o" or i=="O"):
+    
+    #if i in vowels:
         count=count+1
         print (i, end=" ")
 #print ("the vowels in a string are:",count) 
