25407dba8dfd0fd71a09a15039fe917bb6a9889a
emmelance <e.ucunguyab@alustudent.com>
2023-12-20 15:24:42 +0200

    challenge 10

---
+++ challenge10.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#prime numbers 
+Start = int(input("Enter the starting range:"))
+End = int(input("Enter the ending range:"))
+if Start > 1:
+    count = 0
+for i in range (Start, End+1):
+    if i>1:
+        for j in range (2, i):
+            if i % j == 0:
+                break
+        else :
+          print (i, end =" ")
+else:
+    print("this is prime number")
